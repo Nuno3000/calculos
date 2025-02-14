594f2ac16197456134de0ce97ec3daed6aa3cda8
nuno3000 <195126209+Nuno3000@users.noreply.github.com>
2025-02-14 18:09:59 +0000

    Tornar a classe Aritmetica numa classe estática. Criar a classe Conversões e mover o método
     O método  ConverterTemperetura() foi removido da classe Aritmetica e colocado na classe conversões uma vez que implementa uma operação que não se adequa.

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -6,22 +6,14 @@ using System.Threading.Tasks;
 
 namespace Calculos
 {
-    class Aritmetica
+    static class Aritmetica
     {
-        /// <summary>
-        /// Tipo de conversão de temperatura a executar.
-        /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo = 0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-        }
+
         /// <summary>
         /// Operação soma.
         /// </summary>
         /// <returns>Retorna a soma de dois números.</returns>
-        public int Somar(int x, int y)
+        public static int Somar(int x, int y)
         {
             return x + y;
         }
@@ -30,23 +22,10 @@ namespace Calculos
         /// Operação subtração.
         /// </summary>
         /// <returns>Retorna o resultado da subtração de dois números.</returns>
-        public int Subtrair(int x, int y)
+        public static int Subtrair(int x, int y)
         {
             return x - y;
         }
 
-        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.8000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return ((temperatura - 32) / 1.8000);
-            }
-
-            return -1;
-        }
     }
 }
